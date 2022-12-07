f6d8a7b2c973c2b14ac428b6386f8f2f81c4ee96
AlirezaJafarpour82 <117573734+AlirezaJafarpour82@users.noreply.github.com>
2022-12-07 23:18:12 +0330

    Update EX1_3.py

--- EX1/EX1_3.py
+++ EX1/EX1_3.py
@@ -1,1 +1,18 @@
 "Alireza Jafarpour 4012061009
+def ThirdEdge(FirstEdge,secondEdge) :
+    '''
+    (int,int) -> int
+    In this function we will calculate the maximum ThirdEdge of an triangle by having its FirstEdge and secondEdge.
+
+    Examples : 
+    ThirdEdge(8, 10) ➞ 17  
+    ThirdEdge(5, 7) ➞ 11 
+    ThirdEdge(9, 2) ➞ 10
+    '''
+    if FirstEdge>0 and secondEdge>0 :
+        ThirdEdge = ( FirstEdge + secondEdge ) -1
+    else :
+        print('it is impossible')
+        
+    return ThirdEdge
+print(ThirdEdge(-1,4))
