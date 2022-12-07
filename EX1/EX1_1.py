0faa77e22bc063503fe4ff0f2e5f1748c584ea90
AlirezaJafarpour82 <117573734+AlirezaJafarpour82@users.noreply.github.com>
2022-12-07 22:39:52 +0330

    Update EX1_1.py

--- EX1/EX1_1.py
+++ EX1/EX1_1.py
@@ -1,1 +1,13 @@
 "Alireza Jafarpour 4012061009"
+def num(n) :
+	'''
+	(int) -> int
+	In this function we will add one to each given number.
+
+	Examples :
+	num(0) ➞ 1 
+	num(9) ➞ 10 
+	num(-3) ➞ -2
+	'''
+	n+=1
+	return n
