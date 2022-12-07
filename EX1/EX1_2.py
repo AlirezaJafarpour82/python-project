e275506bfb39e9c2aecd620fcc5f09602d581afe
AlirezaJafarpour82 <117573734+AlirezaJafarpour82@users.noreply.github.com>
2022-12-07 23:03:19 +0330

    Update EX1_2.py

--- EX1/EX1_2.py
+++ EX1/EX1_2.py
@@ -1,1 +1,15 @@
 "Alireza Jafarpour 4012061009
+def AreaOfTri(base,height) :
+    '''
+    (int,int) -> int
+    In this function we will calculate the area of an triangle by having its base and height.
+
+    Examples : 
+    AreaOfTri(3,2) ➞ 3 
+    AreaOfTri(7,4) ➞ 14 
+    AreaOfTri(10,10) ➞ 50
+    '''
+
+    area=(base*height)/2
+    return area
+print(AreaOfTri(1,4))
