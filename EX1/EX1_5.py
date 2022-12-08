630987eed531ac2881cdb038274d5f42774fdf75
AlirezaJafarpour82 <117573734+AlirezaJafarpour82@users.noreply.github.com>
2022-12-08 16:25:23 +0330

    Update EX1_5.py

--- EX1/EX1_5.py
+++ EX1/EX1_5.py
@@ -1,1 +1,14 @@
 "Alireza Jafarpour 4012061009
+age=int(input())
+if age<= 1 :
+    print(" you are an infant. ")
+elif age>1 and age<13 :
+    print(" you are a child. ")
+elif age>=13 and age<20 :
+    print(" you are a teenager. ")
+elif age>= 20 :
+    print(" you are an adult. ")
+
+#just for fun 
+else :
+    print(" sorry! you are dead. ")
